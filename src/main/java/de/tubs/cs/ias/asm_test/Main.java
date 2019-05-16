55d4aa8ac022e9c3a66c233c59b1f2f43802609d
David Klein <david.klein@tu-braunschweig.de>
2019-05-16 17:38:41 +0200

    Instrumenting a jar adds required class files
    When instrumenting a jar file, we now automatically add the taint-aware
    string-like class files.

--- src/main/java/de/tubs/cs/ias/asm_test/Main.java
+++ src/main/java/de/tubs/cs/ias/asm_test/Main.java
@@ -6,14 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
+import java.io.*;
 
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
@@ -22,18 +21,23 @@ import java.lang.invoke.MethodHandles;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(description = "Replaces all String instances with taint-aware Strings.",
-         name = "asm_taint", mixinStandardHelpOptions = true, version = "asm_taint 0.0.1")
+        name = "asm_taint", mixinStandardHelpOptions = true, version = "asm_taint 0.0.1")
 public class Main implements Callable<Void> {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    @CommandLine.Option(names = { "-f", "--file" }, required = true, paramLabel = "Input", description = "The input class/jar file")
+    @CommandLine.Option(names = {"-f", "--file"}, required = true, paramLabel = "Input", description = "The input class/jar file")
     private File inputFile;
 
-    @CommandLine.Option(names = { "-o", "--out" }, required = true, paramLabel = "Output", description = "The output class/jar file")
+    @CommandLine.Option(names = {"-o", "--out"}, required = true, paramLabel = "Output", description = "The output class/jar file")
     private File outputFile;
 
-    @CommandLine.Option(names = { "-c", "--check"}, paramLabel = "Check Requirements", description = "Check whether the required class files are present.")
+    @CommandLine.Option(names = {"-c", "--check"}, paramLabel = "Check Requirements", description = "Check whether the required class files are present.")
     private boolean checkRequirements;
+
+    private static final String TStringClassName = "de/tubs/cs/ias/asm_test/IASString.class";
+    private static final String TStringBuilderClassName = "de/tubs/cs/ias/asm_test/IASStringBuilder.class";
+
+    private static final List<String> TStringTypesClassNames = Arrays.asList(TStringClassName, TStringBuilderClassName);
 
     /**
      * Checks whether all the supporting files are present in the output directory.
@@ -43,13 +47,17 @@ public class Main implements Callable<Void> {
      */
     private boolean checkRequirements() {
         File outDir = new File(this.outputFile.getParent());
-        if(!outDir.isDirectory()) { return false; }
-        String[] requiredFilenames = { "IASString.class", "IASStringBuilder.class"};
+        if (!outDir.isDirectory()) {
+            return false;
+        }
+        String[] requiredFilenames = {"IASString.class", "IASStringBuilder.class"};
         File instrumentedDir = new File(outDir, Constants.TPackage);
-        if(!instrumentedDir.isDirectory()) { return false; }
-        for(String required : requiredFilenames) {
+        if (!instrumentedDir.isDirectory()) {
+            return false;
+        }
+        for (String required : requiredFilenames) {
             File requiredFile = new File(instrumentedDir, required);
-            if(requiredFile.exists() && requiredFile.isFile()) {
+            if (requiredFile.exists() && requiredFile.isFile()) {
                 logger.info("Found required file {} at {}", required, requiredFile.getAbsoluteFile());
             } else {
                 logger.error("Did not find required file {} at {}", required, requiredFile.getAbsoluteFile());
@@ -60,81 +68,129 @@ public class Main implements Callable<Void> {
         return true;
     }
 
-    protected void instrumentClassStream(InputStream i, OutputStream o) throws IOException {
-	ClassReader cr = new ClassReader(i);
-	ClassWriter writer = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
-	//ClassVisitor cca = new CheckClassAdapter(writer);
-	ClassTaintingVisitor smr = new ClassTaintingVisitor(writer);
-	cr.accept(smr, 0);
-	o.write(writer.toByteArray());
+    private void instrumentClassStream(InputStream i, OutputStream o) throws IOException {
+        ClassReader cr = new ClassReader(i);
+        ClassWriter writer = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+        //ClassVisitor cca = new CheckClassAdapter(writer);
+        ClassTaintingVisitor smr = new ClassTaintingVisitor(writer);
+        cr.accept(smr, 0);
+        o.write(writer.toByteArray());
     }
 
-    protected void copySingleEntry(InputStream i, OutputStream o) throws IOException {
+    private void copySingleEntry(InputStream i, OutputStream o) throws IOException {
         int len = 0;
         byte[] buffer = new byte[1024];
 
-	while((len = i.read(buffer, 0, buffer.length)) != -1) {
-	    o.write(buffer, 0, len);
-	}
-    }
-
-    protected void instrumentClassFile(File input, File output) throws IOException {
-        FileInputStream fi = new FileInputStream(input);
-	FileOutputStream fo = new FileOutputStream(output);
-	logger.info("Reading class file from: {}", input.getAbsolutePath());
-	this.instrumentClassStream(fi, fo);
-	logger.info("Writing transformed class file to: {}", output.getAbsolutePath());	
-    }
-
-    protected void instrumentJarFile(File input, File output) throws IOException {
-        JarFile ji = new JarFile(input);
-
-	FileOutputStream fos = new FileOutputStream(output);
-	JarOutputStream jos = new JarOutputStream(fos);
-
-	logger.info("Reading jar file from: {}", input.getAbsolutePath());
-
-	for (Enumeration<JarEntry> e = ji.entries(); e.hasMoreElements();) {
-	    JarEntry jei = e.nextElement();
-	    JarEntry jeo = new JarEntry(jei.getName());
-	    InputStream jeis = ji.getInputStream(jei);
-
-	    logger.info("Reading jar entry: {}", jei.getName());
-
-	    jos.putNextEntry(jeo);
-
-	    if (jei.getName().endsWith(".class")) {
-		this.instrumentClassStream(jeis, jos);
-	    } else {
-		this.copySingleEntry(jeis, jos);
-	    }
-            jeis.close();
-            jos.closeEntry();
+        while ((len = i.read(buffer, 0, buffer.length)) != -1) {
+            o.write(buffer, 0, len);
         }
+    }
+
+    private void instrumentClassFile(File input, File output) throws IOException {
+        FileInputStream fi = new FileInputStream(input);
+        FileOutputStream fo = new FileOutputStream(output);
+        logger.info("Reading class file from: {}", input.getAbsolutePath());
+        this.instrumentClassStream(fi, fo);
+        logger.info("Writing transformed class file to: {}", output.getAbsolutePath());
+    }
+
+    private void instrumentJarFile(File input, File output) throws IOException {
+        JarOutputStream jos;
+        String jarPath = getPathToCurrentJar();
+        File f = new File(jarPath);
+        try (JarFile ji = new JarFile(input); JarFile currJar = new JarFile(f)) {
+
+            FileOutputStream fos = new FileOutputStream(output);
+            jos = new JarOutputStream(fos);
+
+            logger.info("Adding required class files to jar..");
+
+            List<JarEntry> entriesToAdd = getJarEntriesToCopy(currJar);
+            for (JarEntry je : entriesToAdd) {
+                if (ji.getJarEntry(je.getName()) != null) {
+                    logger.info("{} is already contained in jar, skipping..", je.getName());
+                    continue;
+                }
+                logger.info("Adding jar entry: {}", je.getName());
+                InputStream currJarIn = currJar.getInputStream(je);
+                JarEntry ne = new JarEntry(je.getName());
+                jos.putNextEntry(ne);
+                this.copySingleEntry(currJarIn, jos);
+                currJarIn.close();
+                jos.closeEntry();
+            }
+
+            logger.info("Reading jar file from: {}", input.getAbsolutePath());
+
+            for (Enumeration<JarEntry> e = ji.entries(); e.hasMoreElements(); ) {
+                JarEntry jei = e.nextElement();
+                JarEntry jeo = new JarEntry(jei.getName());
+                InputStream jeis = ji.getInputStream(jei);
+
+                logger.info("Reading jar entry: {}", jei.getName());
+
+                jos.putNextEntry(jeo);
+
+                // Skip the taint aware string types so we don't mess them up by instrumenting them again!
+                if (jei.getName().endsWith(".class") && !TStringTypesClassNames.contains(jei.getName())) {
+                    this.instrumentClassStream(jeis, jos);
+                } else {
+                    this.copySingleEntry(jeis, jos);
+                }
+                jeis.close();
+                jos.closeEntry();
+            }
+        }
+
         jos.close();
 
-	logger.info("Writing transformed jar file to: {}", output.getAbsolutePath());
+        logger.info("Writing transformed jar file to: {}", output.getAbsolutePath());
     }
 
     @Override
     public Void call() throws IOException {
-        if(this.checkRequirements && !this.checkRequirements()) {
+        if (this.checkRequirements && !this.checkRequirements()) {
             logger.error("Required support files do not exist, can't proceed!");
             return null;
         }
 
-	if (this.inputFile.getName().endsWith(".class")) {
-	    this.instrumentClassFile(this.inputFile, this.outputFile);
-	} else if (this.inputFile.getName().endsWith(".jar")) {
-	    this.instrumentJarFile(this.inputFile, this.outputFile);
-	} else {
-	    logger.error("Input file name must have class or jar extension!");
-	}
+        if (this.inputFile.getName().endsWith(".class")) {
+            this.instrumentClassFile(this.inputFile, this.outputFile);
+        } else if (this.inputFile.getName().endsWith(".jar")) {
+            this.instrumentJarFile(this.inputFile, this.outputFile);
+        } else {
+            logger.error("Input file name must have class or jar extension!");
+        }
 
         return null;
     }
 
-    public static void main(String[] args) {
+    private static boolean isJarEntryToCopy(String name) {
+        String[] toCopy = { /* "de/", "de/tubs/", "de/tubs/cs/", "de/tubs/cs/ias/", "de/tubs/cs/ias/asm_test/", */ "de/tubs/cs/ias/asm_test/IASStringBuilder.class", "de/tubs/cs/ias/asm_test/IASString.class"};
+        for (String e : toCopy) {
+            if (e.equals(name)) return true;
+        }
+        return false;
+    }
+
+    private static List<JarEntry> getJarEntriesToCopy(JarFile ji) {
+
+        List<JarEntry> entries = new ArrayList<>();
+        for (Enumeration<JarEntry> e = ji.entries(); e.hasMoreElements(); ) {
+            JarEntry jei = e.nextElement();
+            if (isJarEntryToCopy(jei.getName())) {
+                entries.add(jei);
+            }
+        }
+        return entries;
+    }
+
+    private static String getPathToCurrentJar() throws UnsupportedEncodingException {
+        String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        return URLDecoder.decode(path, "UTF-8");
+    }
+
+    public static void main(String[] args) throws IOException {
         CommandLine.call(new Main(), args);
     }
 }
