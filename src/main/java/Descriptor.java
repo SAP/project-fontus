760dfdd61a7f2ef1c787edcece296b6f0d609273
David Klein <david.klein@tu-braunschweig.de>
2019-05-16 14:43:40 +0200

    Preparation for large Refactoring
    Added a class to later handle parsing method/type descriptors.
    It will encapsulate the functionality to replace single types.
    Additionally it should help with questions like, where are parameters
    that need special handling (e.g., String to taint-aware String mapping)
    among a larger number of function parameters.
    
    It has quite a few unit tests, so this commit adds junit handling to
    Gradle too.

---
+++ src/main/java/Descriptor.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+class Descriptor {
+
+    private static final Pattern PRIMITIVE_DATA_TYPES = Pattern.compile("[ZBCSIFDJ]");
+    private final Collection<String> parameters;
+    private final String returnType;
+
+    private Descriptor(Collection<String> parameters, String returnType) {
+        this.parameters = parameters;
+        this.returnType = returnType;
+    }
+
+    Descriptor(String[] parameters, String returnType) {
+        this.parameters = Arrays.asList(parameters);
+        this.returnType = returnType;
+    }
+
+    // This is kinda ugly, as a single String parameter would be applicable to the var args overload below..
+    // The java spec tries to resolve overloaded methods without taking var args into account first however,
+    // so a single String argument will result in this constructor being invoked.
+    // See: https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.12.2
+    Descriptor(String returnType) {
+        this.parameters = Collections.emptyList();
+        this.returnType = returnType;
+    }
+
+    @SuppressWarnings("OverloadedVarargsMethod")
+    Descriptor(String ...args) {
+        List<String> arguments = Arrays.asList(args);
+        int lastIndex = arguments.size() - 1;
+        this.returnType = arguments.get(lastIndex);
+        this.parameters = arguments.subList(0, lastIndex);
+    }
+
+    private static String replaceSuffix(String s, String from, String to) {
+        if(s.endsWith(from)) {
+            int lIdx = s.lastIndexOf(from);
+            assert lIdx != -1;
+            String ret = s.substring(0, lIdx) + to;
+            return ret;
+        }
+        return s;
+    }
+
+    public Descriptor replaceType(String from, String to) {
+        Collection<String> replaced = this.parameters.stream().map(str -> replaceSuffix(str, from, to)).collect(Collectors.toList());
+        String ret = replaceSuffix(this.returnType, from, to);
+        return new Descriptor(replaced, ret);
+    }
+
+
+    public Collection<String> getParameters() {
+        return Collections.unmodifiableCollection(this.parameters);
+    }
+
+    public String getReturnType() {
+        return this.returnType;
+    }
+
+    public String toDescriptor() {
+        String params = String.join("", this.parameters);
+        return String.format("(%s)%s", params, this.returnType);
+    }
+
+    @Override
+    public String toString() {
+        String params = String.join(",", this.parameters);
+        return String.format("Descriptor{parameters=[%s], returnType='%s'}", params, this.returnType);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Descriptor that = (Descriptor) obj;
+        return this.parameters.equals(that.parameters) &&
+                this.returnType.equals(that.returnType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.parameters, this.returnType);
+    }
+
+    // TODO: maybe remove the ';'s?
+    // TODO: throw exception on invalid descriptor
+    // TODO: think of a nicer structure, this is really messy
+    public static Descriptor parseDescriptor(String descriptor) {
+        ArrayList<String> out;
+        StringBuilder returnType;
+        try (Scanner sc = new Scanner(descriptor)) {
+            sc.useDelimiter("");
+            String opening = sc.next();
+            assert "(".equals(opening);
+            out = new ArrayList<>();
+            String next = sc.next();
+            StringBuilder buffer = new StringBuilder();
+            boolean inType = false;
+            while (!")".equals(next)) {
+                buffer.append(next);
+                Matcher primitivesMatcher = PRIMITIVE_DATA_TYPES.matcher(next);
+                if (!inType && primitivesMatcher.matches()) {
+                    out.add(buffer.toString());
+                    buffer = new StringBuilder();
+                } else {
+                    inType = true;
+                    if (";".equals(next)) {
+                        out.add(buffer.toString());
+                        buffer = new StringBuilder();
+                        inType = false;
+                    }
+                }
+
+                next = sc.next();
+            }
+
+            returnType = new StringBuilder();
+            while (sc.hasNext()) {
+                returnType.append(sc.next());
+            }
+        }
+
+        return new Descriptor(out, returnType.toString());
+    }
+
+}
---
+++ src/test/java/DescriptorTests.java
@@ -0,0 +1,121 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings({"DuplicateStringLiteralInspection", "SpellCheckingInspection"})
+class DescriptorTests {
+
+    @Test
+    void parseToString() {
+        Descriptor d = new Descriptor("Ljava/lang/String;");
+        Descriptor pd = Descriptor.parseDescriptor("()Ljava/lang/String;");
+        assertEquals(d, pd, "toString should have no parameters and return Ljava/lang/String;");
+    }
+
+    @ParameterizedTest(name = "(){0} = no parameters and {0} as return type.")
+    @ValueSource(strings = {"Z", "B", "C", "S", "I", "F", "D", "J"})
+    void parseNoParamReturnPrimitive(String primitive) {
+        Descriptor d = new Descriptor(primitive);
+        String descriptor = String.format("()%s", primitive);
+        Descriptor pd = Descriptor.parseDescriptor(descriptor);
+        assertEquals(d, pd, String.format("%s should parse to no params and %s as return type.", descriptor, primitive));
+    }
+
+    @ParameterizedTest(name = "({0})V = a {0} parameter and V as return type.")
+    @ValueSource(strings = {"Z", "B", "C", "S", "I", "F", "D", "J"})
+    void parsePrimitiveToVoid(String primitive) {
+        String descriptor = String.format("(%s)V", primitive);
+        Descriptor d = new Descriptor(new String[]{primitive}, "V");
+        Descriptor pd = Descriptor.parseDescriptor(descriptor);
+        assertEquals(d, pd, String.format("'%s' should parse to a '%s' param and 'V' as return type.", descriptor, primitive));
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDescriptorData")
+    void reassembleDescriptor(String descriptor, Descriptor d) {
+        Descriptor pd = Descriptor.parseDescriptor(descriptor);
+        assertEquals(descriptor, pd.toDescriptor(), String.format("%s should be equal to %s", descriptor, pd.toDescriptor()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDescriptorData")
+    void parseDescriptor(String descriptor, Descriptor d) {
+        Descriptor pd = Descriptor.parseDescriptor(descriptor);
+        assertEquals(d, pd, String.format("%s and %s should be equal.", d.toString(), pd.toString()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDescriptorReplacementData")
+    void replaceTypeInDescriptor(Descriptor initial, String from, String to, Descriptor result) {
+        Descriptor rd = initial.replaceType(from, to);
+        assertEquals(rd, result, String.format("After replacing all occurrences of '%s' in '%s' with '%s', the result should be: '%s'!", from, initial, to, result));
+    }
+
+    private static Stream<Arguments> provideDescriptorReplacementData() {
+        String tstringDesc = Constants.TStringDesc + ";"; // TODO: fix globally
+        String stringDesc = String.format("L%s;", Constants.String); // TODO: fix globally
+        return Stream.of(
+                Arguments.of(
+                        new Descriptor("I",
+                                stringDesc,
+                                "[Ljava/lang/Object;",
+                                "F",
+                                "Ljava/util/function/Function;",
+                                "Ljava/lang/Float;",
+                                "Ljava/util/List;"
+                        ),
+                        stringDesc,
+                        tstringDesc,
+                        new Descriptor("I",
+                                tstringDesc,
+                                "[Ljava/lang/Object;",
+                                "F",
+                                "Ljava/util/function/Function;",
+                                "Ljava/lang/Float;",
+                                "Ljava/util/List;"
+                        )
+                ),
+                Arguments.of(
+                        new Descriptor("[Ljava/lang/Object;", "Ljava/util/List;"),
+                        stringDesc,
+                        tstringDesc,
+                        new Descriptor("[Ljava/lang/Object;", "Ljava/util/List;")),
+                Arguments.of(
+                        new Descriptor("[Ljava/lang/String;", "V"),
+                        stringDesc,
+                        tstringDesc,
+                        new Descriptor("[" + tstringDesc, "V")
+                )
+        );
+    }
+
+    private static Stream<Arguments> provideDescriptorData() {
+        return Stream.of(
+                Arguments.of("(ILjava/lang/String;[Ljava/lang/Object;FLjava/util/function/Function;Ljava/lang/Float;)Ljava/util/List;",
+                        new Descriptor("I",
+                                "Ljava/lang/String;",
+                                "[Ljava/lang/Object;",
+                                "F",
+                                "Ljava/util/function/Function;",
+                                "Ljava/lang/Float;",
+                                "Ljava/util/List;"
+                        )),
+                Arguments.of("([Ljava/lang/Object;)Ljava/util/List;",
+                        new Descriptor("[Ljava/lang/Object;", "Ljava/util/List;")),
+                Arguments.of("([Ljava/lang/String;)V",
+                        new Descriptor("[Ljava/lang/String;", "V")),
+                Arguments.of("(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;",
+                        new Descriptor("Ljava/lang/String;",
+                                "[Ljava/lang/Object;",
+                                "Ljava/lang/String;"
+                        ))
+        );
+    }
+}
