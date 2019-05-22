1a3c0219b9d8e33e8578017c82ec2f7d71264e7b
David Klein <david.klein@tu-braunschweig.de>
2019-05-22 16:08:22 +0200

    System Test sources
    Unsure whether committing jar files is optimal.
    The two committed jars are wildly suboptimal, but serve as a decent
    starting point for manual testing.

---
+++ tests/src/ArgumentTests.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+class ArgumentTests {
+
+    public static void main(String[] args) throws Exception {
+
+        File f = new File("/home/leen/.tig_history");
+        Scanner sc = new Scanner(f, "UTF-8");
+        String s = sc.findWithinHorizon(".", 2);
+        System.out.println(s);
+        sc.close();
+    }
+}
