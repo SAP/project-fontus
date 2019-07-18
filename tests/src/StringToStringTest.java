f7272f8b75f35612c360ab1835828cc3692addc9
David Klein <david.klein@tu-braunschweig.de>
2019-07-18 15:40:14 +0200

    Fixes #13
    We have to rename the String.toString() calls when rewriting to
    taint-aware Strings.

---
+++ tests/src/StringToStringTest.java
@@ -0,0 +1,10 @@
+class StringToStringTest {
+
+    public static void main(String[] args) {
+        String s = new String("bonjour");
+        String s2 = s.toString();
+
+        System.out.println("s = " + s);
+        System.out.println("s2 = " + s2);
+    }
+}
