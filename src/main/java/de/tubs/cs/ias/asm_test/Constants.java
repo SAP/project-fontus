df90b1b18a9aef131f27bd2a50545a263c5244d2
David Klein <david.klein@tu-braunschweig.de>
2019-08-01 11:24:58 +0200

    First attempt to fix generic/standard library issue
    Some methods in the standard library return e.g., Set<String>. In the
    bytecode we only see the return of Set. Every method interacting with
    those Sets returns Objects directly followed by a checkcast instruction
    to String.
    
    Before this patch we just rewrote all checkcast instructions casting to
    String to make them cast to taint-aware String. This breaks in this
    instance as we can't cast String to TString.
    
    This change attempts to solve this issue by keeping track of JDK methods
    returning Object directly followed by a checkcast to String. If that is
    detected, the checkcast instruction is replaced by a method call to
    a conversion function which checks for the appropriate conversion for
    the actual type. Namely conversion from String for Strings or a cast for
    taint-aware Strings.

--- src/main/java/de/tubs/cs/ias/asm_test/Constants.java
+++ src/main/java/de/tubs/cs/ias/asm_test/Constants.java
@@ -38,6 +38,8 @@ final class Constants {
      */
     static final String TStringBufferQN = TPackage + "IASStringBuffer";
 
+    static final String TStringUtilsQN = TPackage + "IASStringUtils";
+
     /**
      * Name of the toString method.
      */
---
+++ src/main/java/de/tubs/cs/ias/asm_test/IASStringUtils.java
@@ -0,0 +1,18 @@
+package de.tubs.cs.ias.asm_test;
+
+public final class IASStringUtils {
+
+    public static IASString fromObject(Object obj) {
+        if(obj instanceof String) {
+            return new IASString((String)obj);
+        } else if(obj instanceof IASString) {
+            return (IASString) obj;
+        } else {
+            throw new IllegalArgumentException(String.format("Obj is of type %s, but only String or TString are allowed!", obj.getClass().getCanonicalName()));
+        }
+    }
+
+    private IASStringUtils() {
+
+    }
+}
