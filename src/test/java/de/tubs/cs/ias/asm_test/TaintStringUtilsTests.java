5007842c0555afb8b0d2c7743e8ef4709f0bb014
David Klein <david.klein@tu-braunschweig.de>
2019-08-06 17:24:18 +0200

    Added some TStringUtils tests

---
+++ src/test/java/de/tubs/cs/ias/asm_test/TaintStringUtilsTests.java
@@ -0,0 +1,33 @@
+package de.tubs.cs.ias.asm_test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaintStringUtilsTests {
+    @Test
+    public void testConcat() {
+        String s1 = "x";
+        IASString is1 = new IASString(s1);
+        String s2 = "12";
+        IASString is2 = new IASString(s2);
+        String manual = s1 + " = " + s2;
+        String ifmt = "\u0001 = \u0001";
+        String concatStr = IASStringUtils.concat(ifmt, is2, is1).getString(); // Reverse argument order due to stack machine
+        assertEquals(manual, concatStr, "Manual concat and Tstring.concat should result in the same output");
+    }
+
+    @Test
+    public void testConcatNull() {
+        String s1 = "x";
+        IASString is1 = new IASString(s1);
+        String s2 = null;
+        IASString is2 = null;
+        String manual = s1 + " = " + s2;
+        String ifmt = "\u0001 = \u0001";
+        String concatStr = IASStringUtils.concat(ifmt, is2, is1).getString(); // Reverse argument order due to stack machine
+        assertEquals(manual, concatStr, "Manual concat and Tstring.concat should result in the same output when null is present");
+    }
+}
