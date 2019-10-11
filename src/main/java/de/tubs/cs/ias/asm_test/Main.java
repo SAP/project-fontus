6ed3258464e16c7f9f66d5c5b20a9de8fe13ede7
David Klein <david.klein@tu-braunschweig.de>
2019-10-11 15:40:52 +0200

    Removed the add taint-aware classes to instrumented jar file

--- src/main/java/de/tubs/cs/ias/asm_test/Main.java
+++ src/main/java/de/tubs/cs/ias/asm_test/Main.java
@@ -42,26 +42,10 @@ public final class Main implements Callable<Void> {
     )
     private File outputFile;
 
-    @CommandLine.Option(
-            names = {"-a", "--add"},
-            paramLabel = "Add taint-aware classes",
-            description = "Adds the class files of our taint-aware data types to the instrumented .jar file."
-    )
-    private boolean addTaintAwareClassFiles;
-
-    private static final String TStringClassName = toClassName(Constants.TStringQN);
-    private static final String TStringBuilderClassName = toClassName(Constants.TStringBuilderQN);
-    private static final String TStringBufferClassName = toClassName(Constants.TStringBufferQN);
-    private static final String TReflectionProxies = toClassName("de/tubs/cs/ias/asm_test/IASReflectionProxies");
 
     private static final String classSuffix = ".class";
     private static final String jarSuffix = ".jar";
-    private static final List<String> TStringTypesClassNames = Arrays.asList(
-            TStringClassName,
-            TStringBuilderClassName,
-            TReflectionProxies,
-            TStringBufferClassName
-    );
+
 
     private Main() {
         this.instrumenter = new Instrumenter();
@@ -99,10 +83,6 @@ public final class Main implements Callable<Void> {
             FileOutputStream fos = new FileOutputStream(output);
             jos = new JarOutputStream(fos);
 
-            if (this.addTaintAwareClassFiles) {
-                copyTaintAwareClassFiles(jos, ji, currJar);
-            }
-
             logger.info("Reading jar file from: {}", input.getAbsolutePath());
 
             for (Enumeration<JarEntry> e = ji.entries(); e.hasMoreElements(); ) {
@@ -115,7 +95,7 @@ public final class Main implements Callable<Void> {
                 jos.putNextEntry(jeo);
 
                 // Skip the taint aware string types so we don't mess them up by instrumenting them again!
-                if (jei.getName().endsWith(classSuffix) && !TStringTypesClassNames.contains(jei.getName())) {
+                if (jei.getName().endsWith(classSuffix)) {
                     this.instrumentClassStream(jeis, jos);
                 } else {
                     copySingleEntry(jeis, jos);
@@ -128,25 +108,6 @@ public final class Main implements Callable<Void> {
         jos.close();
 
         logger.info("Writing transformed jar file to: {}", output.getAbsolutePath());
-    }
-
-    private static void copyTaintAwareClassFiles(JarOutputStream jos, JarFile ji, JarFile currJar) throws IOException {
-        logger.info("Adding required class files to jar..");
-
-        List<JarEntry> entriesToAdd = getJarEntriesToCopy(currJar);
-        for (JarEntry je : entriesToAdd) {
-            if (ji.getJarEntry(je.getName()) != null) {
-                logger.info("{} is already contained in jar, skipping..", je.getName());
-                continue;
-            }
-            logger.info("Adding jar entry: {}", je.getName());
-            InputStream currJarIn = currJar.getInputStream(je);
-            JarEntry ne = new JarEntry(je.getName());
-            jos.putNextEntry(ne);
-            copySingleEntry(currJarIn, jos);
-            currJarIn.close();
-            jos.closeEntry();
-        }
     }
 
     private void instrumentDirectory(File input, File output) throws IOException {
@@ -180,25 +141,6 @@ public final class Main implements Callable<Void> {
     public Void call() throws IOException {
         this.walkFileTree(this.inputFile, this.outputFile);
         return null;
-    }
-
-    private static boolean isJarEntryToCopy(String name) {
-        for (String e : TStringTypesClassNames) {
-            if (e.equals(name)) return true;
-        }
-        return false;
-    }
-
-    private static List<JarEntry> getJarEntriesToCopy(JarFile ji) {
-
-        List<JarEntry> entries = new ArrayList<>();
-        for (Enumeration<JarEntry> e = ji.entries(); e.hasMoreElements(); ) {
-            JarEntry jei = e.nextElement();
-            if (isJarEntryToCopy(jei.getName())) {
-                entries.add(jei);
-            }
-        }
-        return entries;
     }
 
     private static String getPathToCurrentJar() throws UnsupportedEncodingException {
