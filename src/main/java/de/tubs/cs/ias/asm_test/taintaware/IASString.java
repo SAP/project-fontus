0ed949ffbca3c2fcff3d43e73593cb05c54c3c34
David Klein <david.klein@tu-braunschweig.de>
2020-01-28 17:30:24 +0100

    Minor fixes wrt failing tests

--- src/main/java/de/tubs/cs/ias/asm_test/taintaware/IASString.java
+++ src/main/java/de/tubs/cs/ias/asm_test/taintaware/IASString.java
@@ -304,7 +304,9 @@ public final class IASString implements IASTaintAware, Comparable<IASString>, Ch
         if (m.find()) {
             taint |= replacement.tainted;
         }
-        return new IASString(this.str.replaceFirst(regex.str, replacement.str), taint);
+        String result = this.str.replaceFirst(regex.str, replacement.str);
+        if(result.isEmpty()) { taint = false; }
+        return new IASString(result, taint);
     }
 
     public IASString replaceAll(IASString regex, IASString replacement) {
@@ -315,7 +317,9 @@ public final class IASString implements IASTaintAware, Comparable<IASString>, Ch
         if (m.find()) {
             taint |= replacement.tainted;
         }
-        return new IASString(this.str.replaceAll(regex.str, replacement.str), taint);
+        String result = this.str.replaceAll(regex.str, replacement.str);
+        if(result.isEmpty()) { taint = false; }
+        return new IASString(result, taint);
     }
 
     public IASString replace(CharSequence target, CharSequence replacement) {
