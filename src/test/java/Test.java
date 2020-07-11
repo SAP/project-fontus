d3ccbecd3f94e37d29b73c4f446183a6363f1dbb
Simon Kurt Petrik <simon.kurt.petrik@sap.com>
2020-07-11 23:48:41 +0200

    * Cleanup

--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-import de.tubs.cs.ias.asm_test.config.Configuration;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(Configuration.getConfiguration().getTaintMethod());
-    }
-}
