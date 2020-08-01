70c37d36f0588a56a17d1c2ffa36634201336427
Simon Kurt Petrik <simon.kurt.petrik@sap.com>
2020-08-01 12:49:01 +0200

    * Cleanup * Reduced error messages

--- src/main/java/de/tubs/cs/ias/asm_test/taintaware/lazycomplex/IASTaintInformation.java
+++ src/main/java/de/tubs/cs/ias/asm_test/taintaware/lazycomplex/IASTaintInformation.java
@@ -1,6 +1,5 @@
 package de.tubs.cs.ias.asm_test.taintaware.lazycomplex;
 
-import de.tubs.cs.ias.asm_test.agent.TaintAgent;
 import de.tubs.cs.ias.asm_test.config.Configuration;
 import de.tubs.cs.ias.asm_test.taintaware.lazycomplex.operations.BaseOperation;
 import de.tubs.cs.ias.asm_test.taintaware.shared.IASTaintRange;
