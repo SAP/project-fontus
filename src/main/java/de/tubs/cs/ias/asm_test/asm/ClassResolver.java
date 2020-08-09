617e1f62d6c8857e092e5507bc6b23c60775b405
Simon Kurt Petrik <simon.kurt.petrik@sap.com>
2020-08-09 22:25:50 +0200

    + Added logging if something during instrumentation failed + Added class resolving with system classloader and thread classloader in ClassResolver + Added class resolving with passed classloader and thread classloader for interface and super class inspection

--- src/main/java/de/tubs/cs/ias/asm_test/asm/ClassResolver.java
+++ src/main/java/de/tubs/cs/ias/asm_test/asm/ClassResolver.java
@@ -2,10 +2,10 @@ package de.tubs.cs.ias.asm_test.asm;
 
 import de.tubs.cs.ias.asm_test.Constants;
 import de.tubs.cs.ias.asm_test.utils.LogUtils;
+import de.tubs.cs.ias.asm_test.utils.ParentLogger;
 import de.tubs.cs.ias.asm_test.utils.Utils;
 
 import java.io.InputStream;
-import de.tubs.cs.ias.asm_test.utils.ParentLogger;
 
 public class ClassResolver {
     private final ClassLoader loader;
@@ -16,11 +16,18 @@ public class ClassResolver {
     }
 
     public InputStream resolve(String className) {
-        if(className.startsWith("[L") && className.endsWith(";")) {
-            className = className.substring(2, className.length()-1);
+        if (className.startsWith("[L") && className.endsWith(";")) {
+            className = className.substring(2, className.length() - 1);
         }
         String fixed = Utils.fixupReverse(className) + Constants.CLASS_FILE_SUFFIX;
         logger.info("Trying to resolve {} from {}", fixed, className);
-        return this.loader.getResourceAsStream(fixed);
+        InputStream is = this.loader.getResourceAsStream(fixed);
+        if (is == null) {
+            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fixed);
+            if (is == null) {
+                is = ClassLoader.getSystemResourceAsStream(fixed);
+            }
+        }
+        return is;
     }
 }
--- src/main/java/de/tubs/cs/ias/asm_test/instrumentation/Instrumenter.java
+++ src/main/java/de/tubs/cs/ias/asm_test/instrumentation/Instrumenter.java
@@ -4,28 +4,28 @@ import de.tubs.cs.ias.asm_test.asm.ClassResolver;
 import de.tubs.cs.ias.asm_test.asm.TypeHierarchyReaderWithLoaderSupport;
 import de.tubs.cs.ias.asm_test.config.Configuration;
 import de.tubs.cs.ias.asm_test.utils.LogUtils;
+import de.tubs.cs.ias.asm_test.utils.ParentLogger;
 import org.mutabilitydetector.asm.NonClassloadingClassWriter;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
 import java.io.IOException;
 import java.io.InputStream;
-import de.tubs.cs.ias.asm_test.utils.ParentLogger;
 
 public class Instrumenter {
     private static final ParentLogger logger = LogUtils.getLogger();
 
     public byte[] instrumentClass(InputStream in, ClassResolver resolver, Configuration config) throws IOException {
-        return instrumentInternal(new ClassReader(in), resolver, config);
+        return instrumentInternal(new ClassReader(in), resolver, config, null);
     }
 
-    public byte[] instrumentClass(byte[] classFileBuffer, ClassResolver resolver, Configuration config) {
-        return instrumentInternal(new ClassReader(classFileBuffer), resolver, config);
+    public byte[] instrumentClass(byte[] classFileBuffer, ClassResolver resolver, Configuration config, ClassLoader loader) {
+        return instrumentInternal(new ClassReader(classFileBuffer), resolver, config, loader);
     }
 
-    private static byte[] instrumentInternal(ClassReader cr, ClassResolver resolver, Configuration config) {
+    private static byte[] instrumentInternal(ClassReader cr, ClassResolver resolver, Configuration config, ClassLoader loader) {
         NonClassloadingClassWriter writer = new NonClassloadingClassWriter(cr, ClassWriter.COMPUTE_FRAMES, new TypeHierarchyReaderWithLoaderSupport(resolver));
-        ClassTaintingVisitor smr = new ClassTaintingVisitor(writer, resolver, config);
+        ClassTaintingVisitor smr = new ClassTaintingVisitor(writer, resolver, config, loader);
         cr.accept(smr, ClassReader.SKIP_FRAMES);
         String clazzName = cr.getClassName();
         String superName = cr.getSuperName();
