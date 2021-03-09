e96c1bd68a989c6cc3a1fa487c90269f9279c383
Patnaik <suv.sanjit.patnaik@sap.com>
2021-03-09 15:10:34 +0100

    clean up unwanted methods

--- src/main/java/de/tubs/cs/ias/asm_test/sql_injection/SQLChecker.java
+++ src/main/java/de/tubs/cs/ias/asm_test/sql_injection/SQLChecker.java
@@ -2,7 +2,6 @@ package de.tubs.cs.ias.asm_test.sql_injection;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tubs.cs.ias.asm_test.sql_injection.antiSQLInjection.antiSQLInjection;
-import de.tubs.cs.ias.asm_test.sql_injection.attack_cases.CommentLineAttack;
 import de.tubs.cs.ias.asm_test.utils.NetworkRequestObject;
 import de.tubs.cs.ias.asm_test.utils.NetworkResponseObject;
 import org.apache.calcite.sql.parser.SqlParser;
@@ -16,19 +15,6 @@ import java.lang.reflect.InvocationTargetException;
 
 public class SQLChecker {
 
-    public static void printCheck(String json_string) throws IOException {
-        JSONObject json_obj = new JSONObject(json_string);
-        String sql_string = json_obj.getString("payload");
-        JSONArray input_ranges = json_obj.getJSONArray("ranges");
-        for (int i = 0; i < input_ranges.length(); i++){
-            int start_index = (int) input_ranges.getJSONObject(i).get("start");
-            int end_index = (int) input_ranges.getJSONObject(i).get("end");
-            String user_input = sql_string.substring(start_index,end_index);
-            System.out.println(user_input);
-            //checkAttack(user_input);
-        }
-    }
-
     public static void checkTaintedString(String tainted_string) throws RuntimeException, IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InterruptedException {
         JSONObject json_obj = new JSONObject(tainted_string);
         String sql_string = json_obj.getString("payload");
@@ -38,14 +24,4 @@ public class SQLChecker {
         NetworkResponseObject.setResponseMessage(req_obj,!json_array.isEmpty());
     }
 
-    private static void checkAttack(String tainted_string){
-        JSONArray attack_results =new JSONArray();
-        attack_results.put(CommentLineAttack.checkCommentLineAttack(tainted_string));
-    }
-
-    public static void main(String[] args) throws IOException {
-        String tainted_json_string = "SELECT * FROM products WHERE id = 10 or 1=1;--";
-
-        //SQLChecker.printCheck(tainted_json_string);
-    }
 }
