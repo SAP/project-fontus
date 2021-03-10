fa21b4684d4e7cddb46a04725b49006196d7673c
I522915 <jennifer.bischoff@sap.com>
2021-03-10 17:59:08 +0100

    Added sanitization abort

--- src/main/java/com/sap/fontus/config/abort/SanitizationAbort.java
+++ src/main/java/com/sap/fontus/config/abort/SanitizationAbort.java
@@ -1,14 +1,10 @@
 package com.sap.fontus.config.abort;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sap.fontus.config.Configuration;
 import com.sap.fontus.taintaware.IASTaintAware;
-import com.sap.fontus.taintaware.range.IASString;
-import com.sap.fontus.taintaware.shared.IASStringable;
 import com.sap.fontus.taintaware.shared.IASTaintRange;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.sap.fontus.sanitizer.Sanitization;
@@ -19,8 +15,11 @@ public class SanitizationAbort extends Abort {
         String taintedString = taintAware.toIASString().getString();
         List<IASTaintRange> ranges = taintAware.toIASString().getTaintRanges();
         System.err.printf("String \"%s\" is tainted and reached sink \"%s\" of category \"%s\"! \n", taintAware, sink, category);
+        // until categories are specified by the taint mechanism, use:
+        List<Sanitization.AttackCategory> categories = new ArrayList();
+        categories.add(Sanitization.AttackCategory.XSS);
         // sanitize here
-        String sanitizedString = "NOT IMPLEMENTED";//Sanitization.sanitizeSinks(taintedString, ranges, List.of(Sanitization.AttackCategory.XSS));
+        String sanitizedString = Sanitization.sanitizeSinks(taintedString, ranges, categories);
         System.err.println(taintedString + " was sanitized and resultet in: " + sanitizedString);
         taintAware.setContent(sanitizedString, ranges);
     }
