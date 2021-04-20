389cd08fce3105022d089e8254f5551625e0540c
Simon Kurt Petrik <d070494@WDFL41000187D.emea.global.corp.sap>
2021-04-20 15:54:47 +0200

    Work in progress

--- src/main/java/com/sap/fontus/manual/spring/converters/XMLBeanClassNameConverter.java
+++ src/main/java/com/sap/fontus/manual/spring/converters/XMLBeanClassNameConverter.java
@@ -2,6 +2,7 @@ package com.sap.fontus.manual.spring.converters;
 
 import com.sap.fontus.Constants;
 import com.sap.fontus.config.Configuration;
+import com.sap.fontus.utils.Utils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
@@ -27,7 +28,7 @@ public class XMLBeanClassNameConverter {
         for (int i = 0; i < attributes.getLength(); i++) {
             Node attribute = attributes.item(i);
             if (Constants.STRING_FULL_NAME.equals(attribute.getNodeValue())) {
-                attribute.setNodeValue(Configuration.getConfiguration().getTaintStringConfig().getTStringQN());
+                attribute.setNodeValue(Utils.slashToDot(Configuration.getConfiguration().getTaintStringConfig().getTStringQN()));
             }
         }
     }
