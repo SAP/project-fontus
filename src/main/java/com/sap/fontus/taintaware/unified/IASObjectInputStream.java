76ee4d5a684c36bbbd5cb14aa00d55efa3967181
David Klein <david.klein@tu-braunschweig.de>
2022-01-24 15:18:44 +0100

    Disabled object resolving for ObjectStreams, we should try to handle this differently, this doesn't work cleanly

--- src/main/java/com/sap/fontus/taintaware/unified/IASObjectInputStream.java
+++ src/main/java/com/sap/fontus/taintaware/unified/IASObjectInputStream.java
@@ -5,6 +5,7 @@ import com.sap.fontus.utils.ConversionUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.util.Properties;
 
 public class IASObjectInputStream extends ObjectInputStream {
     public IASObjectInputStream(InputStream in) throws IOException {
@@ -24,8 +25,12 @@ public class IASObjectInputStream extends ObjectInputStream {
 
     @Override
     protected Object resolveObject(Object obj) throws IOException {
-        Object converted = ConversionUtils.convertToInstrumented(obj);
+        /*if(obj instanceof Properties) {
+            return obj;
+        }*/
+        return obj;
+        //Object converted = ConversionUtils.convertToInstrumented(obj);
         //System.out.printf("Converted: %s -> %s%n", obj.getClass().getName(), converted.getClass().getName());
-        return converted;
+        //return converted;
     }
 }
