deab5d9ff477b09de02e683c3a9fb22404f68926
Thomas Barber <thomas.barber@sap.com>
2022-02-16 19:06:59 +0100

    Unwrap result sets if necessary

--- src/main/java/com/sap/fontus/taintaware/unified/IASResultSetUtils.java
+++ src/main/java/com/sap/fontus/taintaware/unified/IASResultSetUtils.java
@@ -6,9 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class IASResultSetUtils {
+
     public static IASString getString(ResultSet rs, int idx) throws SQLException {
-        if(rs instanceof IASResultSet) {
-           return ((IASResultSet) rs).getTString(idx);
+        if (rs instanceof IASResultSet) {
+            return ((IASResultSet) rs).getTString(idx);
+        } else if (rs.isWrapperFor(IASResultSet.class)) {
+            IASResultSet iasResultSet = rs.unwrap(IASResultSet.class);
+            return iasResultSet.getTString(idx);
         } else {
             String rv = rs.getString(idx);
             return IASString.fromString(rv);
@@ -16,8 +20,11 @@ public class IASResultSetUtils {
     }
 
     public static IASString getString(ResultSet rs, IASString label) throws SQLException {
-        if(rs instanceof IASResultSet) {
+        if (rs instanceof IASResultSet) {
             return ((IASResultSet) rs).getTString(label.getString());
+        } else if (rs.isWrapperFor(IASResultSet.class)) {
+            IASResultSet iasResultSet = rs.unwrap(IASResultSet.class);
+            return iasResultSet.getTString(label.getString());
         } else {
             String rv = rs.getString(label.getString());
             return IASString.fromString(rv);
