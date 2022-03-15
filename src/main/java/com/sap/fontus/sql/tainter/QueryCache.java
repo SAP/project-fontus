e2f19e0bd2f08bdd1160eb158d41de24c40cb40e
Thomas Barber <thomas.barber@sap.com>
2022-03-15 19:39:25 +0100

    Try a caffeine cache for queries

--- src/main/java/com/sap/fontus/sql/tainter/QueryCache.java
+++ src/main/java/com/sap/fontus/sql/tainter/QueryCache.java
@@ -1,5 +1,7 @@
 package com.sap.fontus.sql.tainter;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.sap.fontus.utils.Pair;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
@@ -11,34 +13,35 @@ public enum QueryCache {
     INSTANCE;
     private int hits = 0;
     private int misses = 0;
-    private final WeakHashMap<String, Pair<String, QueryParameters>> queryCache;
-     QueryCache() {
-         this.queryCache = new WeakHashMap<>();
-     }
+    private final Cache<String, Pair<String, QueryParameters>> queryCache;
 
-     public Pair<String, QueryParameters> parseQuery(String query) {
-         Pair<String, QueryParameters> parsed = this.queryCache.get(query);
-         if(parsed != null) {
-             this.hits++;
-             if(this.hits + this.misses % 1000 == 0) {
-                 System.out.printf("QueryCache: %d/%d h/m%n", this.hits, this.misses);
-             }
-             return parsed;
-         }
-         StatementTainter tainter = new StatementTainter();
-         Statements stmts=null;
-         try {
-             stmts = CCJSqlParserUtil.parseStatements(query);
-             stmts.accept(tainter);
-         } catch (JSQLParserException jsqlParserException) {
-             jsqlParserException.printStackTrace();
-         }
-         Pair<String, QueryParameters> pair = new Pair<>(stmts.toString().trim(), tainter.getParameters());
-         this.queryCache.put(query, pair);
-         this.misses++;
-         if(this.hits + this.misses % 1000 == 0) {
-             System.out.printf("QueryCache: %d/%d h/m%n", this.hits, this.misses);
-         }
-         return pair;
-     }
+    QueryCache() {
+        this.queryCache = Caffeine.newBuilder().build();
+    }
+
+    public Pair<String, QueryParameters> parseQuery(String query) {
+        Pair<String, QueryParameters> parsed = this.queryCache.getIfPresent(query);
+        if (parsed != null) {
+            this.hits++;
+            if (this.hits + this.misses % 1000 == 0) {
+                System.out.printf("QueryCache: %d/%d h/m%n", this.hits, this.misses);
+            }
+            return parsed;
+        }
+        StatementTainter tainter = new StatementTainter();
+        Statements stmts = null;
+        try {
+            stmts = CCJSqlParserUtil.parseStatements(query);
+            stmts.accept(tainter);
+        } catch (JSQLParserException jsqlParserException) {
+            jsqlParserException.printStackTrace();
+        }
+        Pair<String, QueryParameters> pair = new Pair<>(stmts.toString().trim(), tainter.getParameters());
+        this.queryCache.put(query, pair);
+        this.misses++;
+        if (this.hits + this.misses % 1000 == 0) {
+            System.out.printf("QueryCache: %d/%d h/m%n", this.hits, this.misses);
+        }
+        return pair;
+    }
 }
