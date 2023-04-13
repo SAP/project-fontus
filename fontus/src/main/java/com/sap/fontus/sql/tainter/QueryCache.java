c8194044e89ca5f59efdd0c42c55a0bf113f98cc
David Klein <david.klein@tu-braunschweig.de>
2023-04-13 11:44:01 +0200

    Exposing SQL tainting hit counts via MXBean

--- fontus/src/main/java/com/sap/fontus/sql/tainter/QueryCache.java
+++ fontus/src/main/java/com/sap/fontus/sql/tainter/QueryCache.java
@@ -2,7 +2,9 @@ package com.sap.fontus.sql.tainter;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.sap.fontus.config.Configuration;
 import com.sap.fontus.utils.Pair;
+import com.sap.fontus.utils.stats.Statistics;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statements;
@@ -12,12 +14,16 @@ public enum QueryCache {
     private int hits = 0;
     private int misses = 0;
     private final Cache<String, Pair<String, QueryParameters>> queryCache;
-
+    private boolean collectStatistics;
     QueryCache() {
+        this.collectStatistics = Configuration.getConfiguration().collectStats();
         this.queryCache = Caffeine.newBuilder().build();
     }
 
     public Pair<String, QueryParameters> parseQuery(String query) {
+        if (this.collectStatistics) {
+            Statistics.INSTANCE.incrementTotalQueries();
+        }
         Pair<String, QueryParameters> parsed = this.queryCache.getIfPresent(query);
         if (parsed != null) {
             this.hits++;
@@ -25,6 +31,9 @@ public enum QueryCache {
                 System.out.printf("QueryCache: %d/%d h/m%n", this.hits, this.misses);
             }
             return parsed;
+        }
+        if (this.collectStatistics) {
+            Statistics.INSTANCE.incrementRewrittenQueries();
         }
         StatementTainter tainter = new StatementTainter();
         Statements stmts = null;
