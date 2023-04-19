90b1f2f242e85348024bae507600d4ca35c29ea9
David Klein <david.klein@tu-braunschweig.de>
2023-04-19 17:27:52 +0200

    Fixed Thread contention for CharArrayTaint restoration

--- fontus/src/main/java/com/sap/fontus/taintaware/unified/IASCharArrayTaint.java
+++ fontus/src/main/java/com/sap/fontus/taintaware/unified/IASCharArrayTaint.java
@@ -1,15 +1,21 @@
 package com.sap.fontus.taintaware.unified;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
 
 // Make this package private
 class IASCharArrayTaint {
 
     // Use a WeakHashMap here so that the entries are removed once the char array goes out of scope
     // Make sure it is synchronized to prevent the keys going out of scope
-    private final Map<char[], IASTaintInformationable> globalCharArrayTaintMap = Collections.synchronizedMap(new WeakHashMap<>());
+    private final Cache<char[], IASTaintInformationable> globalCharArrayTaintMap = Caffeine.newBuilder()
+            .expireAfterAccess(5L, TimeUnit.MINUTES)
+            .build();
 
     private IASCharArrayTaint() {}
 
@@ -24,7 +30,7 @@ class IASCharArrayTaint {
     }
 
     public IASTaintInformationable getTaint(char[] chars) {
-        return this.globalCharArrayTaintMap.get(chars);
+        return this.globalCharArrayTaintMap.getIfPresent(chars);
     }
 
     public IASTaintInformationable setTaint(IASTaintInformationable taint, int srcBegin, int srcEnd, char[] dst, int dstBegin) {
