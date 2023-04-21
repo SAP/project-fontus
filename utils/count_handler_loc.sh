7f1f6f8833122c5729a115ed4f2407989a16dd17
David Klein <david.klein@tu-braunschweig.de>
2023-04-21 21:16:23 +0200

    Updated loc counter

--- utils/count_handler_loc.sh
+++ utils/count_handler_loc.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cloc --quiet --by-file src/main/java/com/sap/fontus/gdpr/openmrs/  src/main/java/com/sap/fontus/gdpr/openolat/ src/main/java/com/sap/fontus/gdpr/broadleaf/
+cloc --quiet --by-file fontus/src/main/java/com/sap/fontus/gdpr/openmrs/ \
+  fontus/src/main/java/com/sap/fontus/gdpr/openolat/ \
+  fontus/src/main/java/com/sap/fontus/gdpr/broadleaf/ \
+  fontus/src/main/java/com/sap/fontus/gdpr/jforum/  \
+fontus/src/main/java/com/sap/fontus/gdpr/ctt/ \
+fontus/src/main/java/com/sap/fontus/gdpr/oh/ \
+fontus/src/main/java/com/sap/fontus/gdpr/sap/
